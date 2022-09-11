b87949fa4cfc48d009265f412c9e4aff67bb9062
Nelson Roncatti <nroncatti@gmail.com>
2022-09-11 15:53:01 -0300

    Tabela de vendas e componente DataPicker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
+
 function App() {
 
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <Header />
+      <NotificationButton />
+      <SalesCard />
+    </>
   )
 }
 
